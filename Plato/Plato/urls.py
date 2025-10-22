c7fc28aa451fb193990986c173da69526c60d78e
Студент группы IS <is@sielom.local>
2025-10-22 14:53:21 +0500

    html has started

--- Plato/Plato/urls.py
+++ Plato/Plato/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Republic import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main', views.main)
 ]
